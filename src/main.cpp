d1a78d442df9be3f0c603f74d7438395d6cb1339
rogeryoungh <rogeryoungh@foxmail.com>
2025-06-05 01:51:03 +0800

    feat: try to  parse safetensor meta json

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
+#include <fstream>
 #include <iostream>
+#include <nlohmann/json.hpp>
 
-int main() {
-  std::cout << "Hello World!" << std::endl;
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " <path_to_safetensors_file>" << std::endl;
+    return 1;
+  }
+
+  const std::string path = argv[1];
+  std::ifstream file(path, std::ios::binary);
+  std::int64_t json_size = 0;
+  file.read(reinterpret_cast<char *>(&json_size), sizeof(json_size));
+  std::cout << "JSON size: " << json_size << std::endl;
+
+  std::vector<char> json_data(json_size);
+  file.read(json_data.data(), json_data.size());
+  nlohmann::json json = nlohmann::json::parse(json_data);
+  std::cout << "JSON content: " << json.dump(4) << std::endl;
+
   return 0;
 }
--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,10 @@
 add_rules("plugin.compile_commands.autoupdate")
 add_rules("mode.release", "mode.debug")
 
+add_requires("nlohmann_json")
+
 target("tiny-llm")
 	set_kind("binary")
+    add_packages("nlohmann_json")
 	set_languages("c++23")
 	add_files("src/**.cpp")
