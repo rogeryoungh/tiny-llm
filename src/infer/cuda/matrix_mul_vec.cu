0384b46ed80a9326941a7d5ddb6b1b4ae5967290
rogeryoungh <rogeryoungh@foxmail.com>
2025-06-15 16:42:02 +0800

    refactor: use float4 to optimize matmul kernel

--- src/infer/cuda/matrix_mul_vec.cu
+++ src/infer/cuda/matrix_mul_vec.cu
@@ -12,14 +12,29 @@ __global__ void matrix_mul_vec_fp32_b_fp16_kernel(const float *a, const half *b,
     return;
 
   const int lane = threadIdx.x;
+  const int m4 = m / 4; // 总是假设 m 是 4 的倍数
   float sum = 0.0f;
-  const half *b_row = b + col * m;
 
-  for (int j = lane; j < m; j += warpSize) {
-    float aj = a[j];
-    float bj = __half2float(b_row[j]);
-    sum += aj * bj;
+  const float4 *a4 = reinterpret_cast<const float4 *>(a);
+  const half2 *b2_row = reinterpret_cast<const half2 *>(b + col * m);
+
+  float4 sum4 = make_float4(0.0f, 0.0f, 0.0f, 0.0f);
+
+  for (int j = lane; j < m4; j += warpSize) {
+    float4 a4j = a4[j];
+    half2 b2j0 = b2_row[j * 2 + 0];
+    half2 b2j1 = b2_row[j * 2 + 1];
+    float2 bj0 = __half22float2(b2j0);
+    float2 bj1 = __half22float2(b2j1);
+    float4 bj = make_float4(bj0.x, bj0.y, bj1.x, bj1.y);
+
+    sum4.x += a4j.x * bj.x;
+    sum4.y += a4j.y * bj.y;
+    sum4.z += a4j.z * bj.z;
+    sum4.w += a4j.w * bj.w;
   }
+
+  sum = sum4.x + sum4.y + sum4.z + sum4.w;
 
   sum = warp_reduce_sum(sum);
 
@@ -40,14 +55,28 @@ __global__ void matrix_mul_vec_bias_fp32_b_fp16_kernel(const float *a, const hal
     return;
 
   const int lane = threadIdx.x;
+  const int m4 = m / 4; // 总是假设 m 是 4 的倍数
   float sum = 0.0f;
-  const half *b_row = b + col * m;
+  const float4 *a4 = reinterpret_cast<const float4 *>(a);
+  const half2 *b2_row = reinterpret_cast<const half2 *>(b + col * m);
 
-  for (int j = lane; j < m; j += warpSize) {
-    float aj = a[j];
-    float bj = __half2float(b_row[j]);
-    sum += aj * bj;
+  float4 sum4 = make_float4(0.0f, 0.0f, 0.0f, 0.0f);
+
+  for (int j = lane; j < m4; j += warpSize) {
+    float4 a4j = a4[j];
+    half2 b2j0 = b2_row[j * 2 + 0];
+    half2 b2j1 = b2_row[j * 2 + 1];
+    float2 bj0 = __half22float2(b2j0);
+    float2 bj1 = __half22float2(b2j1);
+    float4 bj = make_float4(bj0.x, bj0.y, bj1.x, bj1.y);
+
+    sum4.x += a4j.x * bj.x;
+    sum4.y += a4j.y * bj.y;
+    sum4.z += a4j.z * bj.z;
+    sum4.w += a4j.w * bj.w;
   }
+
+  sum = sum4.x + sum4.y + sum4.z + sum4.w;
 
   sum = warp_reduce_sum(sum);
 
